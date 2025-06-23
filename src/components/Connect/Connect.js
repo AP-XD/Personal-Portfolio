2892ca886b134b9dbb9de8098ebf12b4ec695965
AP-XD <arkamtg.pramanik@gmail.com>
2025-06-23 08:16:24 +0530

    Introduce new Logo

--- src/components/Connect/Connect.js
+++ src/components/Connect/Connect.js
@@ -1,11 +1,12 @@
 import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
-import { AiFillGithub, AiFillInstagram } from "react-icons/ai";
-import { FaLinkedinIn, FaTelegram } from "react-icons/fa";
-import { MdEmail } from "react-icons/md";
+import { AiOutlineGithub, AiOutlineInstagram } from "react-icons/ai";
+import { FaLinkedinIn, FaTelegramPlane } from "react-icons/fa";
+import { MdOutlineEmail } from "react-icons/md";
 import Particle from "../Particle";
 import ContactForm from "./ContactForm";
 import "./ContactForm.css";
+import arkaImage from "../../Assets/Arka.png";
 
 function Connect() {
   return (
@@ -24,62 +25,61 @@ function Connect() {
 
         {/* Contact Form Section */}
         <ContactForm />
-
-        {/* Social Media Links */}
-        <Row className="social-media-row">
-          <Col md={12} className="text-center">
-            <ul className="home-about-social-links">
-              <li className="social-icons">
-                <a
-                  href="https://github.com/AP-XD"
-                  target="_blank"
-                  rel="noreferrer"
-                  className="icon-colour home-social-icons"
-                >
-                  <AiFillGithub />
-                </a>
-              </li>
-              <li className="social-icons">
-                <a
-                  href="https://tx.me/AP_XD"
-                  target="_blank"
-                  rel="noreferrer"
-                  className="icon-colour home-social-icons"
-                >
-                  <FaTelegram />
-                </a>
-              </li>
-              <li className="social-icons">
-                <a
-                  href="https://www.linkedin.com/in/arka--pramanik/"
-                  target="_blank"
-                  rel="noreferrer"
-                  className="icon-colour home-social-icons"
-                >
-                  <FaLinkedinIn />
-                </a>
-              </li>
-              <li className="social-icons">
-                <a
-                  href="https://www.instagram.com/p__arka"
-                  target="_blank"
-                  rel="noreferrer"
-                  className="icon-colour home-social-icons"
-                >
-                  <AiFillInstagram />
-                </a>
-              </li>
-              <li className="social-icons">
-                <a
-                  href="mailto:arkamtg.pramanik@gmail.com"
-                  className="icon-colour home-social-icons"
-                >
-                  <MdEmail />
-                </a>
-              </li>
-            </ul>
-          </Col>
-        </Row>
+        
+        {/* Profile Image and Social Icons */}
+        <div className="connect-profile-section">
+          <img
+            src={arkaImage}
+            alt="Arka Pramanik"
+            className="connect-profile-image"
+          />
+          
+          {/* Modern Social Icons */}
+          <div className="connect-social-icons">
+            <a
+              href="https://www.linkedin.com/in/arka--pramanik/"
+              target="_blank"
+              rel="noreferrer"
+              className="connect-social-icon linkedin"
+            >
+              <FaLinkedinIn />
+            </a>
+            
+            <a
+              href="https://github.com/AP-XD"
+              target="_blank"
+              rel="noreferrer"
+              className="connect-social-icon github"
+            >
+              <AiOutlineGithub />
+            </a>
+            
+            <a
+              href="https://tx.me/AP_XD"
+              target="_blank"
+              rel="noreferrer"
+              className="connect-social-icon telegram"
+            >
+              <FaTelegramPlane />
+            </a>
+            
+            <a
+              href="https://www.instagram.com/p__arka"
+              target="_blank"
+              rel="noreferrer"
+              className="connect-social-icon instagram"
+            >
+              <AiOutlineInstagram />
+            </a>
+            
+            <a
+              href="mailto:arkamtg.pramanik@gmail.com"
+              className="connect-social-icon email"
+            >
+              <MdOutlineEmail />
+            </a>
+          </div>
+        </div>
       </Container>
     </Container>
   );
